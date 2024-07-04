857fa7301febcc7c0e9d3ffed2319577cb02e32f
sultanaraziamst <sultanaraziaofficial@gmail.com>
2024-07-04 23:06:31 +0600

    adddata

--- app.py
+++ app.py
@@ -14,3 +14,22 @@ def fetch_stock_data(ticker):
     stock_data = yf.download(ticker, start = '2022-01-01', end = '2024-06-01')
     stock_data.reset_index(inplace=True)
     return stock_data
+
+app.layout  = html.Div([
+    html.H1 ("Stock Market Analysis"),
+
+    html.Div([
+        html.Label("Stock market analysis tool"),
+        html.Div([
+        html.Label("Select Stock:"),
+        dcc.Dropdown(
+            id='stock-dropdown',
+            options=[
+                {'label': 'Apple', 'value': 'AAPL'},
+                {'label': 'Microsoft', 'value': 'MSFT'},
+                {'label': 'Amazon', 'value': 'AMZN'},
+                {'label': 'Google', 'value': 'GOOGL'},
+                {'label': 'Tesla', 'value': 'TSLA'}
+    ])
+
+])
