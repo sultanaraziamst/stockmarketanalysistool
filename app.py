f90699976937d2acb3ace72674ec47372d140c34
sultanaraziamst <sultanaraziaofficial@gmail.com>
2024-07-04 22:59:57 +0600

    update

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ import yfinance as yf
 
 
 app = dash.Dash(__name__)
+
+
+def fetch_stock_data(ticker):
+    stock_data = yf.download(ticker, start = '2022-01-01', end = '2024-06-01')
+    stock_data.reset_index(inplace=True)
+    return stock_data
